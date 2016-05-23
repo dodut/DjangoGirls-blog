943dfdf50de8eb90c656174b9563d9f3be209a83
dodut <dodutmilim@gmail.com>
2016-05-23 15:32:11 +0900

    Modified templates to dislplay posts from database.

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
-
-# Create your views here.
+from django.utils import timezone
+from .models import Post
+#장고ORM을 이용하여 쿼리셋을 화면에 나타낼 수 있게 코딩하는중
 def post_list(request):
-    return render(request, 'blog/post_list.html', {})
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    #쿼리셋을 나타내는 변수를 만드는 중
+    return render(request, 'blog/post_list.html', { 'posts': posts})
+    #'posts':posts로 넘겨주는것 parameter값
