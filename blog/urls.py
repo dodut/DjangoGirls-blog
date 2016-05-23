fba36929e2aac0dd1f63fadc173a02344e2e58a2
dodut <dodutmilim@gmail.com>
2016-05-23 17:01:16 +0900

    update

--- blog/urls.py
+++ blog/urls.py
@@ -4,6 +4,16 @@ from . import views
 urlpatterns = [
     url(r'^$', views.post_list, name='post_list'),
     #post_list라는 name의 View에 문자열의 시작^과 끝$을 검색.
+    url(r'^post/(?P<pk>[0-9]+)/$', views.post_detail, name='post_detail'),
+    #^는 시작/ url이 post를 포함하는 경우
+    #모든변수를 pk변수에 넣어 뷰로 전송하겠다 +는 하나이상의 숫자가 와야함
+    #http://127.0.0.1:8000/post//는 안되지만
+    #http://127.0.0.1:8000/post/1231232/는 가능하다는 이야기
+    # /는 마지막에 /가 한번더 와야한다는 의미
+    # $마지막이라는 의미 뒤로 더이상 문자가 오면 안된다.
+# ex) http://127.0.0.1:8000/post/5/의 경우 pk가 5와 일치하는 view로 정보를 보내게 된다
+
+
 ]
 #자주쓰는 정규표현식
 #^ 문자열이 시작할 떄
--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from .models import Post
+
 #장고ORM을 이용하여 쿼리셋을 화면에 나타낼 수 있게 코딩하는중
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     #쿼리셋을 나타내는 변수를 만드는 중
     return render(request, 'blog/post_list.html', { 'posts': posts})
     #'posts':posts로 넘겨주는것 parameter값
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post':post})
